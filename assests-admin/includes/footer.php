cde164cabc13b212010b89fe6557b6dd9bb20648
Amir1912710 <79427346+Amir1912710@users.noreply.github.com>
2022-01-22 11:44:42 +0200

    Create footer.php

---
+++ assests-admin/includes/footer.php
@@ -0,0 +1,15 @@
+<footer>
+	
+<div class="footer-copyright">
+<div class="container-fluid">
+<div class="row">	
+
+<div class="col-lg-12 text-center">
+<p>&copy;<span class="mx-1">2022</span>OnlineTutoring.com<span class="mx-1">&reg;</span>All rights reserved</p>	
+</div>
+	
+</div>
+</div>
+</div>
+	
+</footer>
